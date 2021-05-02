8193df80697bbd221951c0fdec63397fe73b524b
daorti <daneys90@hotmail.com>
2021-05-02 17:46:29 +0200

    faltaba guardar...

--- application.py
+++ application.py
@@ -1,29 +1,17 @@
-from flask import Flask
-
-# print a nice greeting.
-def say_hello(username = "World"):
-    return '<p>Hello %s!</p>\n' % username
-
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>Subimos otra version del tiron</title> </head>\n<body>'''
-instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
+from flask import Flask, jsonify, request
+from datos_dummy import books
 
 application = Flask(__name__)
 
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
+@application.route('/', methods = ['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+@application.route('/api/v1/resources/books/all', methods = ['GET'])
+def get_all():
+    return jsonify(books)
 
 
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
-
-# run the app.
 if __name__ == "__main__":
 
     application.debug = True
